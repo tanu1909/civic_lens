77bbc4affb391859d1149ed4810b59fc3828bc6b
apoorva3004 <apoorva2306@gmail.com>
2026-01-01 09:24:50 +0530

    Add Supabase setup and map markers

--- src/components/MapBoard.jsx
+++ src/components/MapBoard.jsx
@@ -1,36 +1,32 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const MapBoard = () => {
-  const [position, setPosition] = useState([25.4920, 81.8639]); // fallback
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (pos) => {
-          setPosition([pos.coords.latitude, pos.coords.longitude]);
-        },
-        () => {
-          console.log("Location access denied, using default");
-        }
-      );
-    }
-  }, []);
-
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
-      <MapContainer center={position} zoom={15} style={{ height: "100%", width: "100%" }}>
-        <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+    <MapContainer
+      center={[25.492, 81.863]}
+      zoom={15}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
 
-        <Marker position={position}>
-          <Popup>You are here 📍</Popup>
-        </Marker>
-
-      </MapContainer>
-    </div>
+      <Marker position={[25.492, 81.863]}>
+        <Popup>Test Pin</Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
