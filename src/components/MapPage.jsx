b897d49ecf42ce3072f3bf10eee60269751029a0
apoorva3004 <apoorva2306@gmail.com>
2026-01-04 10:44:07 +0530

    Add map view and login page, update routing and Firebase config

---
+++ src/components/MapPage.jsx
@@ -0,0 +1,12 @@
+import MapView from "../components/MapView";
+
+const MapPage = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold p-4">Local Issues Map</h1>
+      <MapView />
+    </div>
+  );
+};
+
+export default MapPage;
